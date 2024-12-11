1b2340e68421cc0002a082bacb672975297d2023
Louis Lemmel <llemmel@k1r3p14.42mulhouse.fr>
2024-12-11 14:14:56 +0100

    rework init + restart philo routine

--- philo/init.c
+++ philo/init.c
@@ -6,70 +6,91 @@
 /*   By: llemmel <llemmel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 14:56:27 by llemmel           #+#    #+#             */
-/*   Updated: 2024/12/10 16:18:32 by llemmel          ###   ########.fr       */
+/*   Updated: 2024/12/11 14:11:46 by llemmel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-static void	destroy_mutex(t_fork **forks, int n)
+static void	destroy_mutex(t_fork *forks, int n)
 {
 	int	i;
 
 	i = 0;
 	while (i < n)
 	{
-		pthread_mutex_destroy(&(*forks)[i]);
+		pthread_mutex_destroy(&forks[i].mtx);
 		i++;
 	}
+	free(forks);
 }
 
-static int	init_fork(t_fork **forks, int nb_philo)
+static int	init_fork(t_philo_main *philo_main)
 {
 	int	i;
+	int	nb_philo;
 
-	i = 0;
-	*forks = (t_fork *)malloc(nb_philo * sizeof(t_fork));
-	if (!*forks)
+	nb_philo = philo_main->arg.nb_philo;
+	philo_main->forks = (t_fork *)malloc(nb_philo * sizeof(t_fork));
+	if (!philo_main->forks)
 		return (print_error(ALLOC_ERROR, 0));
+	i = 0;
 	while (i < nb_philo)
 	{
-		if (pthread_mutex_init(&(*forks)[i], NULL) != 0)
-		{
-			destroy_mutex(&forks, i);
-			return (free(*forks), 0);
-		}
+		if (pthread_mutex_init(&philo_main->forks[i].mtx, NULL) != 0)
+			return (destroy_mutex(philo_main->forks, i), 0);
 		i++;
 	}
 	return (1);
 }
 
-static int	init_philo(t_philo_main *philo_main)
+static int	init_philo(t_philo_main *philo_main, int index)
+{
+	int	nb_philo;
+
+	nb_philo = philo_main->arg.nb_philo;
+	memset(&philo_main->philos[index], 0, sizeof(t_philo));
+	philo_main->philos[index].index = index;
+	philo_main->philos[index].philo_main = &philo_main;
+	if (pthread_mutex_init(&philo_main->philos[index].mtx, NULL) != 0)
+		return (0);
+	philo_main->philos[index].right = &philo_main->forks[index];
+	if (index == 0)
+		philo_main->philos[index].left = &philo_main->forks[nb_philo - 1];
+	else
+		philo_main->philos[index].left = &philo_main->forks[index - 1];
+	return (1);
+}
+
+static int	clean_philo(t_philo_main *philo_main, int n)
+{
+	int	i;
+
+	i = 0;
+	while (i < n)
+	{
+		pthread_mutex_destroy(&philo_main->philos[i].mtx);
+		i++;
+	}
+	free(philo_main->philos);
+	return (0);
+}
+
+int	init_philos(t_philo_main *philo_main)
 {
 	int	i;
 	int	nb_philo;
 
-	i = -1;
+	i = 0;
 	nb_philo = philo_main->arg.nb_philo;
 	philo_main->philos = (t_philo *)malloc(nb_philo * sizeof(t_philo));
 	if (!philo_main->philos)
-		return (print_error(ALLOC_ERROR, 0));
-	while (++i < nb_philo)
+		return (0);
+	while (i < nb_philo)
 	{
-		memset(&philo_main->philos[i], 0, sizeof(t_philo));
-		if (pthread_mutex_init(&philo_main->philos[i].mtx, NULL) != 0)
-		{
-			while (i >= 0)
-				pthread_mutex_destroy(&philo_main->philos[i--].mtx);
-			return (free(philo_main->philos), 0);
-		}
-		philo_main->philos[i].index = i;
-		philo_main->philos[i].philo_main = &philo_main;
-		philo_main->philos[i].right = &philo_main->forks[i];
-		if (i != 0)
-			philo_main->philos[i].left = &philo_main->forks[i - 1];
-		else
-			philo_main->philos[i].left = &philo_main->forks[nb_philo - 1];
+		if (!init_philo(philo_main, i))
+			return (clean_philo(philo_main, i));
+		i++;
 	}
 	return (1);
 }
@@ -77,18 +98,10 @@ static int	init_philo(t_philo_main *philo_main)
 int	init(t_philo_main *philo_main)
 {
 	if (pthread_mutex_init(&philo_main->mtx, NULL) != 0)
+		return (0);
+	if (!init_fork(philo_main))
 		return (print_error(INIT_ERROR, 0));
-	if (!init_fork(&philo_main->forks, philo_main->arg.nb_philo))
-	{
-		pthread_mutex_destroy(&philo_main->mtx);
-		return (print_error(INIT_ERROR, 0));
-	}
-	if (!init_philo(philo_main))
-	{
-		pthread_mutex_destroy(&philo_main->mtx);
-		destroy_mutex(&philo_main->forks, philo_main->arg.nb_philo);
-		free(philo_main->forks);
-		return (print_error(INIT_ERROR, 0));
-	}
+	if (!init_philos(philo_main))
+		return (destroy_mutex(philo_main->forks, philo_main->arg.nb_philo), 0);
 	return (1);
 }
--- philo/run.c
+++ philo/run.c
@@ -6,7 +6,7 @@
 /*   By: llemmel <llemmel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 15:31:14 by llemmel           #+#    #+#             */
-/*   Updated: 2024/12/10 17:42:59 by llemmel          ###   ########.fr       */
+/*   Updated: 2024/12/11 14:14:21 by llemmel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,76 +14,29 @@
 
 int	print_status(t_philo *philo, char *status)
 {
-	int	err;
-
-	if (!get_int_mutex(&philo->philo_main->mtx, &philo->philo_main->is_running, &err))
-		return (0);
-	pthread_mutex_lock(&philo->philo_main->mtx);
-	printf("%zu %d %s", philo->philo_main->time, philo->index, status);
-	pthread_mutex_unlock(&philo->philo_main->mtx);
+	return (1);
 }
 
 int	eat(t_philo *philo)
 {
-	int	err;
-
-	if (philo->index % 2 == 0)
-		pthread_mutex_lock(&philo->left->mtx);
-	else
-		pthread_mutex_lock(&philo->right->mtx);
-	if (!print_status(philo, FORK_STATUS))
-		return (0);
-	if (philo->index % 2 == 0)
-		pthread_mutex_lock(&philo->right->mtx);
-	else
-		pthread_mutex_lock(&philo->left->mtx);
-	if (!print_status(philo, FORK_STATUS))
-		return (0);
-	if (!print_status(philo, EAT_STATUS))
-		return (0);
-	usleep(philo->philo_main->arg.time_to_eat * 1000);
-	pthread_mutex_unlock(&philo->left->mtx);
-	pthread_mutex_unlock(&philo->right->mtx);
 	return (1);
 }
 
 int	sleep(t_philo *philo)
 {
-	if (!print_status(philo, SLEEP_STATUS))
-		return (0);
-	usleep(philo->philo_main->arg.time_to_sleep * 1000);
 	return (1);
 }
 
 int	think(t_philo *philo)
 {
-	usleep(100);
 	return (1);
 }
 
 void	*routine(void *arg)
 {
 	t_philo	*philo;
-	int		err;
 
 	philo = (t_philo *)arg;
-	if (!set_int_mutex(&philo->mtx, &philo->is_ready, 1))
-		return ((void *)print_error("LOCK OR UNLOCK MUTEX ERROR", 0));
-	while (1)
-	{
-		if (get_int_mutex(&philo->philo_main->mtx, &philo->philo_main->is_running, &err))
-			break ;
-		if (err == 1)
-			break ;
-	}
-	if (err == 1)
-		return ((void *)print_error("LOCK OR UNLOCK MUTEX ERROR", 0));
-	if (!eat(philo))
-		return (NULL);
-	if (!sleep(philo))
-		return (NULL);
-	if (!think(philo))
-		return (NULL);
 	return (NULL);
 }
 
@@ -96,7 +49,8 @@ static int	create_threads(t_philo_main *philo_main)
 	nb_philo = philo_main->arg.nb_philo;
 	while (i < nb_philo)
 	{
-		if (pthread_create(&philo_main->philos[i].th, NULL, &routine, &philo_main->philos[i]) != 0)
+		if (pthread_create(&philo_main->philos[i].th, NULL, &routine, \
+			&philo_main->philos[i]) != 0)
 			return (print_error(THREAD_ERROR, 0));
 		i++;
 	}
@@ -107,6 +61,7 @@ int	run(t_philo_main *philo_main)
 {
 	if (!create_threads(philo_main))
 		return (0);
-	// need to join threads
+	if (!join_threads(philo_main))
+		return (0);
 	return (1);
 }
