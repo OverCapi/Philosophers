0fb09ce1369210e60780ea81fe3b51064faeaf42
Louis Lemmel <llemmel@k1r3p10.42mulhouse.fr>
2024-12-17 15:59:04 +0100

    pb ft_usleep

--- philo/run/philos_routine.c
+++ philo/run/philos_routine.c
@@ -6,7 +6,7 @@
 /*   By: llemmel <llemmel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 14:52:59 by llemmel           #+#    #+#             */
-/*   Updated: 2024/12/17 14:53:27 by llemmel          ###   ########.fr       */
+/*   Updated: 2024/12/17 15:55:50 by llemmel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ void	*alone_routine(void *arg)
 	set_int_mutex(&philo->mtx, &philo->is_ready, 1);
 	if (!wait_start(philo))
 		return (NULL);
+	pthread_mutex_lock(philo->right_fork);
 	if (!print_status(philo, FORK_STATUS))
 		return (NULL);
 	while (!is_finished(philo))
